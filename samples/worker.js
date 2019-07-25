9239a06ce90f8e8b52a79ab1418351943228cb91
j.faassen <j.faassen@linkorb.com>
2019-07-25 10:49:25 +0200

    feat: configurable WORKER_QUEUE_ENDPOINT

--- samples/worker.js
+++ samples/worker.js
@@ -1,6 +1,7 @@
 const request = require('request');
+const workerQueueEndpoint = process.env.WORKER_QUEUE_ENDPOINT;
 
-request({ url: 'http://127.0.0.1:4000/jobs/fetch' }, (e, res, body) => {
+request({ url: workerQueueEndpoint + '/jobs/fetch' }, (e, res, body) => {
   console.log('Fetching last job:', res.statusCode, res.statusMessage);
 
   if (!body) return;
@@ -8,7 +9,7 @@ request({ url: 'http://127.0.0.1:4000/jobs/fetch' }, (e, res, body) => {
   console.log(body);
 
   request({
-    url: `http://127.0.0.1:4000/jobs/${JSON.parse(body).id}/response`,
+    url: workerQueueEndpoint + `/jobs/${JSON.parse(body).id}/response`,
     method: 'post',
     json: { status: 'success' }
   }, (e, res) => console.log('Response on job completion:',
